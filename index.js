443c63d8e49705c59b75a852a3123d3199ff3875
orozmat <dj.orozmat.kd@gmail.com>
2024-03-15 13:43:46 +0300

    lesson_13.r

---
+++ index.js
@@ -0,0 +1,22 @@
+
+function createSquare(color) {
+  const square = document.createElement('div');
+  square.classList.add('square', color);
+  return square;
+}
+
+function createChessboard(rows, cols) {
+  const chessboard = document.getElementById('chessboard');
+  chessboard.innerHTML = '';
+  
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      const color = (i + j) % 2 === 0 ? 'white' : 'black';
+      const square = createSquare(color);
+      chessboard.appendChild(square);
+    }
+    chessboard.appendChild(document.createElement('br')); 
+  }
+}
+
+createChessboard(8, 8);
